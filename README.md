ac6386f7ba281761c43c223497d76387d64c34af
Randy Valis <randy.valis@gmail.com>
2017-03-08 17:40:22 -0500

    Using custom lein-lux jar

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # LuxAndroidTest
 Tests using Lux to build an Android app
+
+### Building
+* Unfortunately, I couldn't get the build working with the lein-lux plugin.
+I had to modify the plugin to get it to build correctly with gradle and the support libraries. 
+
+Included in the project is the custom lein-luxc jar file that needs to be used during the build phase. 
+
+Building should be accomplished with
+```
+./gradlew installDebug
+```
+The lux compilation step should be performed after compiling any Java files in the project.
--- app/project.clj
+++ app/project.clj
@@ -1,6 +1,7 @@
-(defproject java_interop "0.1.0-SNAPSHOT"
+(defproject android-test "0.1.0-SNAPSHOT"
   :plugins [[com.github.luxlang/lein-luxc "0.5.0"]]
-  :resource-paths ["src" "/home/randy/Android/Sdk/platforms/android-25/android.jar" 
+  :resource-paths ["src"
+                   "/home/randy/Android/Sdk/platforms/android-25/android.jar" 
                    "build/intermediates/classes/debug/"
                    "build/intermediates/exploded-aar/com.android.support/appcompat-v7/25.1.1/jars/classes.jar"
                    "build/intermediates/exploded-aar/com.android.support/support-fragment/25.1.1/jars/classes.jar"
@@ -16,13 +17,14 @@
         ; :android {:sdk "/home/randy/Android/Sdk"
         ;           :version 25
         ;           :resources ["res" "temp/appcompat/res"]
-        ;           :support-lib "/home/randy/Android/Sdk/extras/android/m2repository/com/android/support/appcompat-v7/25.1.0/appcompat-v7-25.1.0-sources.jar"
-        ;           ;; :support-lib "temp/classes.jar"
+        ;           :manifest "src/main/AndroidManifest.xml"
+        ;           ; :support-lib "/home/randy/Android/Sdk/extras/android/m2repository/com/android/support/appcompat-v7/25.1.0/appcompat-v7-25.1.0-sources.jar"
         ;           :keystore {
         ;             :path "/home/randy/.android/debug.keystore"
         ;             :alias "androiddebugkey"
         ;             :password "android"
         ;                      }
+        ;           }
   }
   
   :source-paths ["src/main"]
